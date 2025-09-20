5f2f4625164a75ee32582784c5ea3cfbc730994b
Рыбина Лидия <lidiar@rambler.ru>
2025-09-20 15:49:43 +0500

    adding func sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -9,9 +9,21 @@ def filter_by_state(input_list: list[dict], state = 'EXECUTED') -> list:
     return  [n_dict for n_dict in input_list if n_dict['state'] == state]
 
 
+def sort_by_date(input_list: list[dict], sorting = False) -> list:
+    """функция сортирует принимаемы список по дате
+    :param input_list:  список словарей
+    :param sorting: необязательный параметр, задающий порядок сортировки (по умолчанию — убывание)
+    """
+    return sorted(input_list, key = lambda n_dict : n_dict['date'], reverse = not sorting)
+
+
 lists = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
      {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
      {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
      {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
 
-print(filter_by_state(lists, 'CANCELED'))
+# print(filter_by_state(lists, 'CANCELED'))
+# print(sort_by_date(lists, 'True'))
+
+
+
