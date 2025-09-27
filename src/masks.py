74cb7a41628b49710c46ab75ac2380aa326a8d2f
Рыбина Лидия <lidiar@rambler.ru>
2025-09-27 19:13:40 +0500

    add test_get_mask_card_number_correct_len and test_get_mask_card_number_correct_type , test_get_mask_card_number_correct_empty

--- src/masks.py
+++ src/masks.py
@@ -5,8 +5,21 @@ def get_mask_card_number(numbers: int) -> Union[str]:
     """Функция маскировки номера банковской карты в виде XXXX XX** **** XXXX, где
     X — это цифра номера.
     :rtype: Union[str]"""
+    # проверка на тип
+    for arg in [numbers]:
+        if not isinstance(arg, int):
+            raise TypeError('Ошибка типа данных')
+
+    # проверка на пустую строку
+    if numbers == 0 or numbers == None:
+        raise TypeError('Номер карты не может быть пустым')
 
     str_num = str(numbers)
+
+    # проверка на корректность длины
+    if len(str_num) != 16:
+        raise ValueError('Длина номера карты не равна 16')
+
     return f"{str_num[0:4]} {str_num[4:6]}** **** {str_num[-4:]}"
 
 
--- tests/test_function.py
+++ tests/test_function.py
@@ -2,11 +2,11 @@ import pytest
 
 from src.masks import get_mask_card_number
 
-"""Функция get_mask_card_number:
-1 Тестирование правильности маскирования номера карты.
-2 Проверка работы функции на различных входных форматах номеров карт, включая граничные случаи и 
-  нестандартные длины номеров.
-3 Проверка, что функция корректно обрабатывает входные строки, где отсутствует номер карты."""
+# """Функция get_mask_card_number:
+# 1 Тестирование правильности маскирования номера карты.
+# 2 Проверка работы функции на различных входных форматах номеров карт, включая граничные случаи и
+#   нестандартные длины номеров.
+# 3 Проверка, что функция корректно обрабатывает входные строки, где отсутствует номер карты."""
 
 @pytest.mark.parametrize(
     "numbers,  expected",
@@ -18,3 +18,34 @@ from src.masks import get_mask_card_number
 def test_get_mask_card_number(numbers, expected):
     assert get_mask_card_number(numbers) == expected
 
+
+@pytest.mark.parametrize(
+    "numbers",
+    [(70070922896063261),
+     (770070922896063261),
+     (70003432606362)]
+)
+def test_get_mask_card_number_correct_len(numbers):
+    with pytest.raises(ValueError):
+        get_mask_card_number(numbers)
+
+
+@pytest.mark.parametrize(
+    "numbers",
+    [(77007092289606.3),
+    (0000000000000000),
+    ('7700709228960632')]
+)
+def test_get_mask_card_number_correct_type(numbers):
+    with pytest.raises(TypeError):
+        get_mask_card_number(numbers)
+
+
+@pytest.mark.parametrize(
+    "numbers",
+    [( ),
+     (0)]
+)
+def test_get_mask_card_number_correct_empty(numbers):
+    with pytest.raises(TypeError):
+        get_mask_card_number(numbers)
