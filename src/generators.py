3f7f8c42a469e629140842368f88777d0838fb24
Рыбина Лидия <lidiar@rambler.ru>
2025-10-05 00:53:55 +0500

    update generators.py

--- src/generators.py
+++ src/generators.py
@@ -1,19 +1,34 @@
-def filter_by_currency(transactions: list[dict], currency: str = 'USD'):
-    result = []
+from typing import Any
 
+
+def filter_by_currency(transactions: list[dict], currency: str = 'USD')   ->  Any | None :
+    """
+    Функция возвращает итератор, который поочередно выдает транзакции, где валюта операции соответствует заданной
+    :param transactions: список словарей, представляющих транзакции.
+    :param currency:валюта операции
+    :return: итератор, который поочередно выдает транзакции, где валюта операции соответствует заданной
+    """
     if len(transactions) == 0 or  transactions is None:
         print('Список транзакций пуст')
 
-
     for current_dict in transactions:
-        try:
-            value = current_dict.get("operationAmount").get("currency").get("code")
-            if value is None:
-                 return "Информация о сумме операции не найдена"
-        except:
+        operation_amount = current_dict.get("operationAmount")
+        if operation_amount is not None:
+            currency_info = operation_amount.get("currency")
+            if currency_info is not None:
+                code = currency_info.get("code")
+                if code is None:
+                    return "Информация о сумме операции не найдена"
+            else:
+                return "Информация о валюте не найдена"
+        else:
             return "Информация о сумме операции не найдена"
+        # except:
+        #     return "Информация о сумме операции не найдена"
 
         for transaction in transactions:
-            if transaction.get("operationAmount").get("currency").get("code") == currency:
+            if transaction["operationAmount"]["currency"]["code"] == currency:
                 yield  transaction
-    return  result
+    return  []
+
+
