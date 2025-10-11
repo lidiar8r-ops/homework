c08f79cde277bd7a10bcc2b55baa144118335cd6
Рыбина Лидия <lidiar@rambler.ru>
2025-10-12 02:08:58 +0500

    update decorators and test

--- src/decorators.py
+++ src/decorators.py
@@ -1,50 +1,50 @@
-#Этот модуль будет использоваться для размещения декораторов
+# Этот модуль будет использоваться для размещения декораторов
 # Ожидаемый вывод в лог-файл mylog.txt
 # при успешном выполнении:  my_function ok
 # Ожидаемый вывод при ошибке: my_function error: тип ошибки. Inputs: (1, 2), {}
 # Где тип ошибки заменяется на текст ошибки.
+import os
 from datetime import datetime
-from functools import  wraps
+from functools import wraps
+from typing import Any, Callable
 
 
-def log(filename=''):
-    def decorator_log(func):
+def log(filename: str = "") -> Callable[..., Any]:
+    def decorator_log(func: Callable[..., Any]) -> Callable[..., Any]:
         @wraps(func)
-        def wrapper(*args, **kwargs):
+        def wrapper(*args: Any, **kwargs: Any) -> Any:
             current_time_start = datetime.now().time()
 
             try:
-                result = func(*args, **kwargs)
+                func(*args, **kwargs)
                 current_time_stop = datetime.now().time()
-                result_bool = 'ok'
-                str_result_2 = f'{func.__name__} {result_bool}'
+                result_bool = "ok"
+                str_result_2 = f"{func.__name__} {result_bool}"
             except Exception as e:
                 result_bool = f"error: ({e})"
                 current_time_stop = datetime.now().time()
-                str_result_2 = f'{func.__name__} {result_bool}. Inputs: {args}, {kwargs}'
+                str_result_2 = f"{func.__name__} {result_bool}. Inputs: {args}, {kwargs}"
 
+            str_result_1 = f"\nНачало выполнения функции : {current_time_start}\n"
+            str_result_3 = f"\nвремя завершения работы функции {current_time_stop}"
 
-            # str_result = f'Начало выполнения функции : {current_time_start}\n{func.__name__} {result_bool}. Inputs: {args}, {kwargs} \nвремя завершения работы функции {current_time_stop}'
-            str_result_1 = f'\nНачало выполнения функции : {current_time_start}\n'
-            str_result_3 = f'\nвремя завершения работы функции {current_time_stop}'
-
-            if filename == '':
+            if filename == "":
                 print(str_result_1)
                 print(str_result_2)
                 print(str_result_3)
             else:
-                with open(filename, 'a', encoding='utf-8') as file:
-                    file.write(str_result_1)
-                    file.write(str_result_2)
-                    file.write(str_result_3)
+                try:
+                # if not os.path.exists(filename) and os.path.isfile(filename):
+                #     raise FileNotFoundError(f"Path {filename} does not exist")
+                    with open(filename, "a", encoding="utf-8") as file:
+                        file.write(str_result_1)
+                        file.write(str_result_2)
+                        file.write(str_result_3)
+                except FileNotFoundError as e:
+                    raise FileNotFoundError(e)
 
-            return  str_result_2
+            return str_result_2
+
         return wrapper
+
     return decorator_log
-
-
-@log(filename="..\\data\\mylog.txt")
-def my_function(x, y):
-    return x / y
-
-my_function(1, 0)
--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -2,6 +2,7 @@ import os
 
 from src.decorators import log
 
+
 @log()
 def my_function(x, y):
     return x + y
@@ -12,29 +13,33 @@ def test_log_ok(capsys):
     captured = capsys.readouterr()
     assert captured.out == "my_function ok"
 
+
 def test_log_error(capsys):
     my_function(1, 2)
     captured = capsys.readouterr()
     assert captured.out == "my_function error: (division by zero). Inputs: (1, 0), {}"
 
 
-name_file = ("..\\data\\mylog.txt"
-@log(filename=name_file))
+name_file = "..\\data\\mylog.txt"
+
+
+name_file = "..\\data\\mylog.txt"
+@log(filename=name_file)
 def my_function_del(x, y):
     return x / y
 
 
 def test_log_ok_file(capsys):
     my_function(1, 2)
-    # name_file = "..\\data\\mylog.txt"
 
-    name_dir = os.path.dirname(os.path.dirname(__file__).rstrip("/"))
-
-    name_file = os.path.join(name_dir, "data", 'mylog.txt')
+    # name_dir = os.path.dirname(os.path.dirname(__file__).rstrip("/"))
+    #
+    # name_file = os.path.join(name_dir, "data", 'mylog.txt')
     print(name_file)
-    # with open(name_file, "r") as f:
-    #     text_file = f.read()
+    with open(name_file, "r") as f:
+        text_file = f.read()
     assert "my_function ok" in text_file
+
 
 #
 # @log(filename="..\\data\\mylog.txt")
