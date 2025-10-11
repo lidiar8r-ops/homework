689551dd477b2db101c781b26580026c73a554de
Рыбина Лидия <lidiar@rambler.ru>
2025-10-11 13:50:59 +0500

    update test_decorators.py

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -17,9 +17,15 @@ def test_log_ok(capsys):
 def my_function_del(x, y):
     return x / y
 
+def test_log_err(capsys):
+    my_function_del(1, 0)
+    captured = capsys.readouterr()
+    assert captured.out == "my_function_del error: (division by zero). Inputs: (1, 0), {}\n"
+
+
 def test_log_error(capsys):
     with pytest.raises(Exception, match="my_function_del error: (division by zero). Inputs: (1, 0), {}"):
         my_function_del(1, 0)
         captured = capsys.readouterr()
-
+        captured.out
 
