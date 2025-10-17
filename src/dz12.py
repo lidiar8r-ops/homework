fadbc7914f7301458712aba749290ae59c297f44
Рыбина Лидия <lidiar@rambler.ru>
2025-10-18 00:36:24 +0500

    add

--- src/dz12.py
+++ src/dz12.py
@@ -1,9 +1,18 @@
-import requests
+import os
+import  json
 
 
 def get_weather(city: str):
-    with open("\data\weather.json", "r", encoding="utf-8") as f:
-        load()
-     return request
+    with open("..\\data\\weather.json", "r", encoding="utf-8") as f:
+        data = json.load(f)
+        day_temperature = [value for value in  data[city].values() ]
+        #print(day_temperature)
+        #with open(f"..\\data\\weather_{city}.json", "w", encoding="utf-8") as f:
+        with open("..\\data\\weather_city.json", "w", encoding="utf-8") as f:
+            json.dump(sum(day_temperature) / 7, f, ensure_ascii=False, indent=4)
+        return sum(day_temperature) / 7
 
 
+
+
+print(get_weather("Moscow"))
