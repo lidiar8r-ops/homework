65398d8088ee19ebec82ee76a5a6b2192a14e209
Рыбина Лидия <lidiar@rambler.ru>
2025-10-18 00:49:23 +0500

    add

--- src/dz12.py
+++ src/dz12.py
@@ -8,8 +8,9 @@ def get_weather(city: str):
         day_temperature = [value for value in  data[city].values() ]
         #print(day_temperature)
         #with open(f"..\\data\\weather_{city}.json", "w", encoding="utf-8") as f:
+        data_city = { f"{city}": f"{sum(day_temperature) / 7}"}
         with open("..\\data\\weather_city.json", "w", encoding="utf-8") as f:
-            json.dump(sum(day_temperature) / 7, f, ensure_ascii=False, indent=4)
+            json.dump(data_city, f, ensure_ascii=False, indent=4)
         return sum(day_temperature) / 7
 
 
