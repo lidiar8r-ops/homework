8a11091a09471bbb7666a76e5e1c3f5d3b287f4f
Рыбина Лидия <lidiar@rambler.ru>
2025-10-19 20:14:52 +0500

    update utils.py

---
+++ src/utils.py
@@ -0,0 +1,20 @@
+import json
+
+
+def get_list_transaction(path_file: str)->list :
+    """
+    принимает на вход путь до JSON-файла и возвращает список словарей с данными о финансовых транзакциях.
+    :param path_file: путь до JSON-файла
+    """
+    data = list()
+    try:
+        with open(path_file, encoding='utf-8') as f:
+            data = json.load(f)
+        return data
+    except json.decoder.JSONDecodeError:
+        return []
+    except FileNotFoundError:
+        return []
+
+
+print(get_list_transaction(r'..\data\operations.json'))
