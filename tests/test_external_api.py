bf175583b776ca23573ef140bec23c6ef2ff66de
Рыбина Лидия <lidiar@rambler.ru>
2025-10-19 23:47:36 +0500

    update test_external_api.py

--- tests/test_external_api.py
+++ tests/test_external_api.py
@@ -1,16 +1,22 @@
 from unittest.mock import patch, Mock
-import requests
+import pytest
 from src.external_api import get_currency_exchange
 
 
-@patch('requests.get')
-def test_euro_to_rub_conversion(self, mock_get):
+
+@pytest.fixture
+def mock_request():
+    with patch('requests.get') as mocked_get:
+        yield mocked_get
+
+
+def test_euro_to_rub_conversion(mock_request):
     """Проверка правильной конвертации EUR->RUB"""
     # Настройка моков (фиктивных запросов)
     mock_response = Mock()
     mock_response.status_code = 200
     mock_response.json.return_value = {'result': 80.0}
-    mock_get.return_value = mock_response
+    mock_request.return_value = mock_response
 
     # Пример транзакции в евро
     transaction = {
@@ -24,9 +30,10 @@ def test_euro_to_rub_conversion(self, mock_get):
 
     expected_result = 80.0
     actual_result = get_currency_exchange(transaction)
-    self.assertAlmostEqual(actual_result, expected_result)
+    assert abs(expected_result - actual_result) < 0.001  # сравнение чисел с плавающей точкой
 
-def test_ruble_amount(self):
+
+def test_ruble_amount():
     """Проверка правильности обработки рублей (без обращения к API)"""
     transaction = {
         "operationAmount": {
@@ -39,9 +46,10 @@ def test_ruble_amount(self):
 
     expected_result = 100.0
     actual_result = get_currency_exchange(transaction)
-    self.assertEqual(actual_result, expected_result)
+    assert expected_result == actual_result
 
-def test_zero_amount(self):
+
+def test_zero_amount():
     """Проверка реакции на операцию с нулевым значением"""
     transaction = {
         "operationAmount": {
@@ -54,14 +62,14 @@ def test_zero_amount(self):
 
     expected_result = 0
     actual_result = get_currency_exchange(transaction)
-    self.assertEqual(actual_result, expected_result)
+    assert expected_result == actual_result
 
-@patch('requests.get')
-def test_http_error(self, mock_get):
+
+def test_http_error(mock_request):
     """Проверка реакции на сетевую ошибку (код статуса отличающийся от 200)"""
     mock_response = Mock()
     mock_response.status_code = 404
-    mock_get.return_value = mock_response
+    mock_request.return_value = mock_response
 
     transaction = {
         "operationAmount": {
@@ -74,17 +82,20 @@ def test_http_error(self, mock_get):
 
     expected_result = 0
     actual_result = get_currency_exchange(transaction)
-    self.assertEqual(actual_result, expected_result)
+    assert expected_result == actual_result
 
+@pytest.fixture
+def mock_request():
+    with patch('requests.get') as mocked_get:
+        yield mocked_get
 
-@patch('requests.get')  # Патчим реальный объект `requests.get`
-def test_json_decode_error(self, mock_get):
+def test_json_decode_error(mock_request):
     """Проверка реакции на некорректный JSON от API."""
-    # Создаем поддельный объект ответа
+    # Создание мока ответа
     mock_response = Mock()
-    mock_response.status_code = 200  # Статус успешный, но тело JSON неправильное
-    mock_response.json.side_effect = json.decoder.JSONDecodeError('', '', 0)  # Исключение при попытке парсинга JSON
-    mock_get.return_value = mock_response  # Возвращаем подделанный ответ
+    mock_response.status_code = 200  # статус успешный, но JSON неправильный
+    mock_response.json.side_effect = json.decoder.JSONDecodeError('', '', 0)  # Исключение при чтении JSON
+    mock_request.return_value = mock_response  # возвращаем подставленный ответ
 
     # Транзакция в валюте EUR
     transaction = {
@@ -97,5 +108,5 @@ def test_json_decode_error(self, mock_get):
     }
 
     # Ожидается исключение JSONDecodeError
-    with raises(json.decoder.JSONDecodeError):
+    with pytest.raises(json.decoder.JSONDecodeError):
         get_currency_exchange(transaction)
