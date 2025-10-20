f9d9121af1f13b34b24655c44ef80d8679f37bd8
Рыбина Лидия <lidiar@rambler.ru>
2025-10-20 21:36:56 +0500

    update lgger in utils.py

--- src/utils.py
+++ src/utils.py
@@ -3,13 +3,11 @@ import logging
 from typing import Any
 
 logger_utils = logging.getLogger('utils')
-file_handler_utils = logging.FileHandler('..\logs\utils.log')
+file_handler_utils = logging.FileHandler('.\\logs\\utils.log', encoding='utf-8')
 file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 file_handler_utils.setFormatter(file_formatter)
 logger_utils.addHandler(file_handler_utils)
 logger_utils.setLevel(logging.DEBUG)
-
-
 
 
 def get_list_transaction(path_file: str) -> list[Any]:
@@ -20,10 +18,17 @@ def get_list_transaction(path_file: str) -> list[Any]:
     """
     data = list()
     try:
+        logger_utils.info("Открытие JSON-файла")
         with open(path_file, encoding="utf-8") as f:
             data = json.load(f)
+
+        logger_utils.info("Получение списка словарей с данными о финансовых транзакциях из JSON-файла")
         return list(data)
+
     except json.decoder.JSONDecodeError:
+        logger_utils.error("Невозможно декодировать JSON-данные")
         return []
+
     except FileNotFoundError:
+        logger_utils.error(f"Не найден JSON-файл {path_file}")
         return []
